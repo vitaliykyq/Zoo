a2386d74d57a0cf1dd0868a551bf691e89cbcdb3
Vitaliy Kybych <vitaliykyq@gmail.com>
2021-05-11 18:17:37 +0300

    test2

--- src/main/java/edu/coursework/zoo/ZooApplication.java
+++ src/main/java/edu/coursework/zoo/ZooApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZooApplication {
-
+    //Some Edit
     public static void main(String[] args) {
         SpringApplication.run(ZooApplication.class, args);
     }
